df6fea19db2b00ee28c3270259ad90ce355b0ada
NBNgoc-2k1 <bongmasanco934@gmail.com>
2023-02-20 09:09:21 +0700

    Create and config project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+import './tailwind.css'
+
+function App() {
+
+
+  return (
+    <div className="bg-light-silver">
+      <p className='text-center text-3xl uppercase'>helloworld</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        brown: '#936464',
+        'dark-grey': '#474747',
+        teal: '#649393',
+        white: '#FFFFFF',
+        'light-silver': '#F5F5F5',
+        'dark-silver': '#D9D9D9',
+        'placeholder':'#979797'
+      }
+    },
+  },
+  plugins: [],
+}
